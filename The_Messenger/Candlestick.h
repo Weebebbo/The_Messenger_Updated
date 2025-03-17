33db84c66b94c9f3c5d5d71c656c669a6ddd6300
Pierpaolo Simoni <ironmanas8@gmail.com>
2025-03-17 10:09:56 +0100

    Update Candlestick.h

--- The_Messenger/Candlestick.h
+++ The_Messenger/Candlestick.h
@@ -1,9 +1,6 @@
 //
 // Created by webubbu on 17/03/25.
 //
-
-#ifndef CANDLESTICK_H
-#define CANDLESTICK_H
 #pragma once
 #include "CollidableObject.h"
 
@@ -15,19 +12,9 @@ namespace agp
 class agp::Candlestick : public CollidableObject
 {
 private:
-    Candlestick(Scene* scene, const RectF& rect, Sprite* sprite, int layer = 0);
-    virtual ~Candlestick() {}
+
 
 public:
-    // extends game logic (-physics, -collisions)
-    virtual void update(float dt) override {RenderableObject::update(dt);}
+    Candlestick(Scene* scene, const RectF& rect, Sprite* sprite, int layer = 0);
 
-    virtual std::string name() override {
-        return strprintf("StaticObject[%d]", _id);
-    }
-
-    virtual bool collision(CollidableObject* with, bool begin, Direction fromDir) override;
 };
-
-
-#endif //CANDLESTICK_H
